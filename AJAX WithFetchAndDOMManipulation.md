8e14ad949713750a39df3e50abcff871cc2140b6
Martin Sander-Thomsen <cph-ms782@cphbusiness.dk>
2019-09-25 21:15:05 +0200

    wednesday exercises

--- AJAX WithFetchAndDOMManipulation.md
+++ AJAX WithFetchAndDOMManipulation.md
@@ -1,8 +1,10 @@
 ## [FetchAndPromises](https://docs.google.com/document/d/1_PkGqF-1MVt0sFDR90ARJlUhQ8DsyC4L12NI6E-iWMc/edit#)
 ### AJAX with fetch and DOM manipulation
-Finding individual jokes
-Adding new Jokes
-Small application to display a quote of the hour
+##### Finding individual jokes  
+
+##### Adding new Jokes  
+
+##### Small application to display a quote of the hour  
 
  1. Remove all html (and only this) inside the div with the container class  (in index.htm in the public folder)  
  2. Add a button to index.html + an empty div-tag, both with id’s so they are easy to “find”  
@@ -10,10 +12,17 @@ Small application to display a quote of the hour
  3. In index.js remove all code meant for the initial joke-sample, add an event listener to the button’s click event and pass a callback that will update the div tag in the index.html with a new quote.  
 
  4. Initially, fetch the quote from a remote API: https://studypoints.info/jokes/api/jokes/period/hour  
+_{  
+  "id": 2,  
+  "joke": "A SQL query goes into a bar, walks up to two tables and asks, \"Can I join you?\"",  
+  "reference": "Unknown"  
+}_
  
 5. Use fetch() to get the quote.  
  
 6. Use developer-tools in your browser and it’s network options to monitor the AJAX-request. Explain why, what you did above, is even possible, when we know the Same Origin Policy governs when/where AJAX-request can go  
+_**Request:** Cross-Origin Resource Sharing (CORS) is specified in the request.  
+**Response:** Access-Control-Allow-Origin is set to * **_
 
 7. Change the functionality to get a new quote every hour. (Hint: use setInterval() )  
 
--- AJAX WithFetchAndDOMManipulation/src/index.js
+++ AJAX WithFetchAndDOMManipulation/src/index.js
@@ -1,8 +1,48 @@
 import 'bootstrap/dist/css/bootstrap.css'
 import jokes from "./jokes";
 
-const allJokes = jokes.getJokes().map(joke => "<li>"+joke+"</li>");
-document.getElementById("jokes").innerHTML = allJokes.join("");
+// const allJokes = jokes.getJokes().map(joke => "<li>"+joke+"</li>").join("");
+// document.querySelector("#jokes").innerHTML = allJokes;
 
+//  implement functionality (in index.js) to find a joke, given it’s id.
+// document.querySelector("#getJokeButton").addEventListener("click", clicky);
+// function clicky(){
+//     var getJokeInput = document.querySelector("#inputGetJoke").value;
+//     document.querySelector("#jokes").innerHTML = jokes.getJokeById(getJokeInput);
+// };
 
+// add the necessary changes to add new jokes to the internal joke-facade.
+// document.querySelector("#setJokeButton").addEventListener("click", clickySetJoke);
+// function clickySetJoke(){
+//     var setJokeInput = document.querySelector("#inputSetJoke").value;
+//     jokes.addJoke(setJokeInput);
+//     document.querySelector("#jokes").innerHTML = "Joke added";
+// };
 
+// document.querySelector("button").addEventListener("click", motD);
+document.querySelector("button").addEventListener("click", timer); 
+
+function motD(){
+    fetch("https://studypoints.info/jokes/api/jokes/period/hour")
+    .then(res => res.json())
+    .then(data => {
+        document.querySelector("#div").innerHTML =data.joke;
+  })
+  
+};
+
+function timer(){
+    window.setTimeout(motD, 3000);
+};
+
+// Add the necessary event handlers, so when pressing each of the “hearts”,
+// it will write the  message North, South, East or West respectively.
+document.querySelector("#east").addEventListener("click", clickIreland); 
+document.querySelector("#north").addEventListener("click", clickIreland); 
+document.querySelector("#west").addEventListener("click", clickIreland); 
+document.querySelector("#south").addEventListener("click", clickIreland); 
+
+function clickIreland(e){
+    document.querySelector("#div").innerHTML = e.target.parentElement.id;
+     
+};
